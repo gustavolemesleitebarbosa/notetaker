027f192eec718b87b9365bf747f2a965248c683e
GustavoLemesLB <gustavo.lemes@work.co>
2023-04-19 17:01:16 -0300

    plus one commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { Session } from 'next-auth';
 import { getServerSession } from "next-auth/next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import { useState, type CSSProperties } from "react";
+import { useEffect, useRef, useState, type CSSProperties } from "react";
 import { authOptions } from "~/server/auth";
 import { api, type RouterOutputs } from "~/utils/api";
 
@@ -25,11 +25,11 @@ const override: CSSProperties = {
 };
 type Note = RouterOutputs["note"]["getAll"][0]
 
-export type Props ={
-  session: Session| null
+export type Props = {
+  session: Session | null
 }
 
-const Home: NextPage<Props> = ({session} : Props) => {
+const Home: NextPage<Props> = ({ session }: Props) => {
   return (
     <>
       <Head>
@@ -38,8 +38,8 @@ const Home: NextPage<Props> = ({session} : Props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Header session ={session}/>
-        <Content  />
+        <Header session={session} />
+        <Content />
       </main>
     </>
   );
@@ -53,6 +53,7 @@ const Content: React.FC = () => {
 
   const { data: sessionData, status: sessionStatus } = useSession()
   const [selectedTopic, setSelectedTopic] = useState<Topic | null>()
+  const [inputContent, setInputContent] = useState("")
 
   const { data: topics, refetch: refetchTopics, isLoading } = api.topic.getAll.useQuery(
     undefined,
@@ -82,10 +83,13 @@ const Content: React.FC = () => {
       );
       return { previousTopics }
     },
+    onSuccess(newTopic) {
+      setSelectedTopic(newTopic)
+    },
     onError: (_err, _newTopic, context) => {
       queryClient.setQueryData([topicsListKey], context?.previousTopics)
     },
-    onSettled() {
+    onSettled(_err, newTopic) {
       void refetchTopics()
     },
   })
@@ -146,18 +150,26 @@ const Content: React.FC = () => {
     }
   })
 
-  if(sessionStatus === 'unauthenticated'){
-    return <button  onClick={() => void signIn()} className="flex items-center justify-center col-span-4 w-full h-[80vh] text-4xl">
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (topics?.length === 0 && inputRef.current) {
+      inputRef.current?.focus();
+    }
+  }, [topics]);
+
+  if (sessionStatus === 'unauthenticated') {
+    return <button onClick={() => void signIn()} className="flex items-center justify-center col-span-4 w-full h-[80vh] text-4xl">
       Please sign in
     </button>
   }
-   
+
   return (
     <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
       {
-        !topics  ?
+        !topics ?
           <div className="flex items-center justify-center col-span-4 h-[80vh]">
-            
+
             <ClipLoader
               color={"##0679FE"}
               loading={true}
@@ -179,24 +191,36 @@ const Content: React.FC = () => {
                       </a>
                     </li>
                   ))}
-                </ul> : <>  {  'Loading topics...'}</>
+                </ul> : <>  {'Loading topics...'}</>
               }
               <div className="divider"></div>
-              <input
-                type="text"
-                placeholder="New Topic"
-                className="input-bordered input input-sm w-full"
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    createTopic.mutate({
-                      title: e.currentTarget.value,
-                    })
-                    e.currentTarget.value = ""
-                  }
-                }}
-              />
+    
+              <div className='relative'>
+              <div className='relative left-4 top-10'>
+              {topics.length === 0 && inputContent === "" &&
+              <span className="absolute flex w-8 h-8 right-0 bottom-7 z-1900 ">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                <span className="relative top-2 left-2 inline-flex rounded-full h-4 w-4 bg-sky-500"></span>
+              </span>}
+              </div>
+                <input
+                  type="text"
+                  placeholder="New Topic"
+                  ref={inputRef}
+                  onChange={(e) => { setInputContent(e.target.value) }}
+                  className={`input-bordered input input-sm w-full ${topics.length === 0 && inputContent === "" ? "border-black-500  " : ""} `}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      createTopic.mutate({
+                        title: e.currentTarget.value,
+                      })
+                      e.currentTarget.value = ""
+                    }
+                  }}
+                />
+              </div>
             </div>
-            <div className="col-span-3">
+            {topics.length > 0 ? <div className="col-span-3">
               <div>
                 {notes?.map((note) => (
                   <div key={note.id} className=" mt-5">
@@ -216,7 +240,18 @@ const Content: React.FC = () => {
                   })
                 }}
               />
-            </div>
+            </div> :
+              <div className="col-span-3 text-align: center">
+                <div className="w-full h-[35vh] flex flex-col flex-wrap justify-center items-center text-4xl ">
+                  <div className='mb-5 text-center' >
+                    You have no topics yet.😔
+                  </div>
+                  <div className='text-center mb-5' >
+                    Please enter your first topic on the input on the left
+                  </div>
+                </div>
+              </div>
+            }
           </>
       }
     </div>
